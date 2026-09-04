9762b38cf17d31793b1d7c43eee6e7e4b0c49b77
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-09-04 01:54:07 -0300

    Removendo arquivos sem uso

--- src/mnist/index.ts
+++
@@ -1,1 +0,0 @@
-export { MnistLoader } from './MnistLoader';
--- src/neural-network/index.ts
+++
@@ -1,5 +0,0 @@
-export { Network } from './Network';
-export { Layer } from './Layer';
-export { Neuron } from './Neuron';
-export { crossEntropy, crossEntropyBatch, mse } from './Loss';
-export type { NetworkConfig, TrainingData, TrainingResult, Prediction } from './types';
